7d3eb722c1f19e86f3779cde53a0945e498a4390
Yannick Epstein <yannick.epstein@gmail.com>
2019-03-13 09:22:26 +0100

    Added function templates for start/end activity insertion and evaluation of places

--- pm4py/algo/discovery/est_miner/utils/est_utils.py
+++ pm4py/algo/discovery/est_miner/utils/est_utils.py
@@ -1,3 +1,40 @@
-def infer_start_activities(log):
+from enum import Enum
 
-def infer_end_activities(log):
+class PlaceFitnessState(Enum):
+    FITTING = 1
+    OVERFED = 2
+    UNDERFED = 3
+
+def insert_unique_start_and_end_activity(log):
+    """
+    Insert a unique start and end activity for each trace in the log.
+
+    Parameters:
+    ---------
+    log: :class:`pm4py.log.log.EventLog`
+            The event log of traces
+    
+    Returns:
+    ---------
+    - The log with the transformed traces
+    - The unique start activity
+    - The unique end activity
+    """
+
+def evaluate_place(log, input_trans, output_trans, tau):
+    """
+    Evaluate if a given place is underfed or overfed based on replaying
+    the log.
+
+    Parameters:
+    ---------
+    log :class:`pm4py.log.log.EventLog`
+            Event log to use for replay
+    input_trans: Set of input transitions of the place
+    output_trans: Set of output transitions of the place
+    tau: Noise filtering parameter of the algorithm
+
+    Returns:
+    ---------
+    A collection of the place's fitness states.
+    """
