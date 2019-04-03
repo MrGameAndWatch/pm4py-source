df7ec3908df623b6c7fc65308597705c5a64d6c6
Yannick Epstein <yannick.epstein@gmail.com>
2019-04-03 20:14:12 +0200

    Updated modelling of orders

--- pm4py/algo/discovery/est_miner/utils/activity_order.py
+++ pm4py/algo/discovery/est_miner/utils/activity_order.py
@@ -1,59 +1,41 @@
-class ActivityOrder:
+class LargerActivityStack:
 
-    def __init__(self, smaller, larger):
-        self.__smaller = smaller
-        self.__larger  = larger
+    def __init__(self):
+        self.__larger_activities = list()
     
-    def is_smaller(self, a1, a2):
-        return a1 in self.__smaller[a2]
+    def push(self, activity):
+        self.__larger_activities.append(activity)
     
-    def is_larger(self, a1, a2):
-        return a1 in self.__larger[a2]
+    def pop(self):
+        return self.__larger_activities.pop()
+    
+    def peek(self):
+        return self.__larger_activities[self.size - 1]
+    
+    def size(self):
+        return len(self.__larger_activities)
+    
+    def to_set(self):
+        return set(self.__larger_activities)
 
 class ActivityOrderBuilder:
 
-    def __init__(self, activities):
-        self.__activities = activities
-        self.__smaller = dict()
-        self.__larger = dict()
+    def __init__(self):
+        self.__is_larger_relations = dict()
     
-    @property
-    def activities(self):
-        return self.__activities
-    
-    def smaller_than(self, a1, a2):
-        """
-        Stores that activity a1 is smaller than
-        activity a2.
-
-        Parameters:
-        -----------
-        a1 - the first activity
-        a2 - the second activity
-
-        Result:
-        -----------
-        a1 < a2 is stored
-        """
-        assert(a1 in self.activities)
-        assert(a2 in self.activities)
-
-        if (a2 not in self.__smaller):
-            self.__smaller[a2] = set()
-        self.__smaller[a2].append(a1)
-        if (a1 not in self.__larger):
-            self.__larger[a1] = a2
+    def add_relation(self, larger=None, smaller=None):
+        if smaller not in self.__is_larger_relations:
+            self.__is_larger_relations[smaller] = LargerActivityStack()
+        self.__is_larger_relations[smaller].push(larger)
     
     def get_ordering(self):
-        self.assert_consistent_ordering()
-        return ActivityOrder(self.__smaller, self.__larger)
+        return ActivityOrder(self.__is_larger_relations)
+
+class ActivityOrder:
+
+    def __init__(self, relations):
+        self.__is_larger_relations = relations
     
-    def assert_consistent_ordering(self):
-        """
-        Asserts that the order is consistent.
-        """
-        for a in self.activities:
-            for b in self.__smaller[a]:
-                assert(a in self.__larger[b])
-                assert(a not in self.__smaller[b])
-                assert(b not in self.__larger[a])
+    @property
+    def is_larger_relations(self):
+        return self.__is_larger_relations
