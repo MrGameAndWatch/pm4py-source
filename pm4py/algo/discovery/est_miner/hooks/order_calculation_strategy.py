e2efd4c691587da3e9f59dd8c0d794bceac1b01f
Yannick Epstein <yannick.epstein@gmail.com>
2019-04-06 12:47:56 +0200

    Fixed order

--- pm4py/algo/discovery/est_miner/hooks/order_calculation_strategy.py
+++ pm4py/algo/discovery/est_miner/hooks/order_calculation_strategy.py
@@ -33,12 +33,13 @@ class LexicographicalOrderStrategy(OrderCalculationStrategy):
 
     def execute(self, log, key):
         activites = log_util.get_event_labels(log, key)
-        input_order_builder  = ActivityOrderBuilder()
-        output_order_builder = ActivityOrderBuilder()
+        input_order_builder  = ActivityOrderBuilder(activites)
+        output_order_builder = ActivityOrderBuilder(activites)
 
         sorted_activities = sorted(activites, key=str.lower)
         for i in range(0, len(sorted_activities)):
             for j in range(i, len(sorted_activities)):
-                input_order_builder.add_relation(larger=sorted_activities[j], smaller=sorted_activities[i])
-                output_order_builder.add_relation(larger=sorted_activities[j], smaller=sorted_activities[i])
+                if (sorted_activities[i] != sorted_activities[j]):
+                    input_order_builder.add_relation(larger=sorted_activities[j], smaller=sorted_activities[i])
+                    output_order_builder.add_relation(larger=sorted_activities[j], smaller=sorted_activities[i])
         return (input_order_builder.get_ordering(), output_order_builder.get_ordering())
