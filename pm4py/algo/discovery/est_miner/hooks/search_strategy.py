a7203a4ab4ae1da5f1cd360352a3dc1b5540c349
Yannick Epstein <yannick.epstein@gmail.com>
2019-04-06 12:50:20 +0200

    Fixed recursion and memory behavior in the dfs

--- pm4py/algo/discovery/est_miner/hooks/search_strategy.py
+++ pm4py/algo/discovery/est_miner/hooks/search_strategy.py
@@ -1,4 +1,5 @@
 import abc
+import copy
 
 import pm4py.objects.log.util.log as log_util
 from pm4py.algo.discovery.est_miner.utils.place import Place
@@ -12,7 +13,7 @@ class SearchStrategy(abc.ABC):
         self,
         log,
         key,
-        parameters,
+        tau,
         pre_pruning_strategy,
         in_order,
         out_order
@@ -37,15 +38,16 @@ class RestrictedRedTreeDfsStrategy(SearchStrategy):
         fitting_places = list()
         roots = self.__get_roots(log, key, pre_pruning_strategy) # list of places
         for root in roots:
-            fitting_places.extend( self.__traverse_red_tree(log, tau, root, in_order, out_order, pre_pruning_strategy) )
-            fitting_places.extend( self.__traverse_blue_tree(log, tau, root, in_order, out_order, pre_pruning_strategy) )
+            fitting_places.extend( self.__traverse_red_tree(log, tau, key, root, in_order, out_order, pre_pruning_strategy) )
+            fitting_places.extend( self.__traverse_blue_tree(log, tau, key, root, in_order, out_order, pre_pruning_strategy) )
+        return fitting_places
 
     def __get_roots(self, log, key, pre_pruning_strategy):
         roots = list()
         activites = log_util.get_event_labels(log, key)
         for a1 in activites:
             for a2 in activites:
-                p = Place([a1], [a2])
+                p = Place({a1}, {a2})
                 if not pre_pruning_strategy.execute(p):
                     roots.append(p)
         return roots
@@ -55,7 +57,7 @@ class RestrictedRedTreeDfsStrategy(SearchStrategy):
         for a in activties:
             if a_max == None:
                 a_max = a
-            elif a in order.is_larger_relations[a_max].to_set():
+            elif a in set(order.is_larger_relations[a_max]):
                 a_max = a
         return a_max
     
@@ -63,33 +65,44 @@ class RestrictedRedTreeDfsStrategy(SearchStrategy):
         self,
         log,
         tau,
+        key,
         root,
         in_order,
         out_order,
         pre_pruning_strategy
     ):
-        if len(root.output_trans) != 1: # restrict red edges
-            return list()
-
         if pre_pruning_strategy.execute(root):
             return list()
 
-        if PlaceFitnessEvaluator.evaluate_place_fitness(log, root, tau) == PlaceFitness.OVERFED:
+        if len(root.output_trans) != 1: # restrict red edges
             return list()
 
+        place_fitness_states = PlaceFitnessEvaluator.evaluate_place_fitness(log, root, tau, key)
         fitting_places = list()
+        if PlaceFitness.FITTING in place_fitness_states:
+            fitting_places.append(root)
+        
+        if PlaceFitness.OVERFED in place_fitness_states:
+            return fitting_places
+
         a_max = self.__get_max_element(root.input_trans, in_order)
-        while (in_order.is_larger_relations[a_max].size() > 0):
-            extended_input_trans = root.input_trans.append( in_order.is_larger_relations[a_max].pop() )
-            new_root = Place(extended_input_trans, root.output_trans)
-            fitting_places.extend( self.__traverse_red_tree(log, tau, new_root, in_order, out_order, pre_pruning_strategy) )
-            fitting_places.extend( self.__traverse_blue_tree(log, tau, new_root, in_order, out_order, pre_pruning_strategy) )
+        larger_elements = copy.copy(out_order.is_larger_relations[a_max])
+        while (len(larger_elements) > 0):
+            extended_input_trans = copy.copy(root.input_trans)
+            new_element = larger_elements.pop()
+            if new_element in extended_input_trans:
+                continue
+            extended_input_trans.add( new_element )
+            new_root = Place(extended_input_trans, copy.copy(root.output_trans))
+            fitting_places.extend( self.__traverse_red_tree(log, tau, key, new_root, in_order, out_order, pre_pruning_strategy) )
+            fitting_places.extend( self.__traverse_blue_tree(log, tau, key, new_root, in_order, out_order, pre_pruning_strategy) )
         return fitting_places
 
     def __traverse_blue_tree(
         self,
         log,
         tau,
+        key,
         root,
         in_order,
         out_order,
@@ -98,24 +111,34 @@ class RestrictedRedTreeDfsStrategy(SearchStrategy):
         if pre_pruning_strategy.execute(root):
             return list()
         
-        if PlaceFitnessEvaluator.evaluate_place_fitness(log, root, tau) == PlaceFitness.UNDERFED:
-            return list()
-
+        place_fitness_states = PlaceFitnessEvaluator.evaluate_place_fitness(log, root, tau, key)
         fitting_places = list()
+        if PlaceFitness.FITTING in place_fitness_states:
+            fitting_places.append(root)
+
+        if PlaceFitness.UNDERFED in place_fitness_states:
+            return fitting_places 
+
         a_max = self.__get_max_element(root.input_trans, out_order)
-        while (out_order.is_larger_relations[a_max].size() > 0):
-            extended_output_trans = root.output_trans.append( out_order.is_larger_relations[a_max].pop() )
-            new_root = Place(root.input_trans, extended_output_trans)
-            fitting_places.extend( self.__traverse_red_tree(log, tau, new_root, in_order, out_order, pre_pruning_strategy) )
-            fitting_places.extend( self.__traverse_blue_tree(log, tau, new_root, in_order, out_order, pre_pruning_strategy) )
+        larger_elements = copy.copy(out_order.is_larger_relations[a_max])
+        while (len(larger_elements) > 0):
+            extended_output_trans = copy.copy(root.output_trans)
+            new_element = larger_elements.pop()
+            if new_element in extended_output_trans:
+                continue
+            extended_output_trans.add(new_element)
+            new_root = Place(copy.copy(root.input_trans), extended_output_trans)
+            fitting_places.extend( self.__traverse_red_tree(log, tau, key, new_root, in_order, out_order, pre_pruning_strategy) )
+            fitting_places.extend( self.__traverse_blue_tree(log, tau, key, new_root, in_order, out_order, pre_pruning_strategy) )
         return fitting_places
 
 class NoSearchStrategy(SearchStrategy):
 
     def execute(
         self, 
-        log, 
-        parameters,
+        log,
+        key,
+        tau,
         pre_pruning_strategy,
         in_order,
         out_order
