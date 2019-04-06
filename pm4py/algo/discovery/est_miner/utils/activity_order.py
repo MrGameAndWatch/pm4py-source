120c1cbde6003a6d18cf35b0c27b4f3a61246466
Yannick Epstein <yannick.epstein@gmail.com>
2019-04-06 12:51:08 +0200

    Simplified order model

--- pm4py/algo/discovery/est_miner/utils/activity_order.py
+++ pm4py/algo/discovery/est_miner/utils/activity_order.py
@@ -1,32 +1,10 @@
-class LargerActivityStack:
-
-    def __init__(self):
-        self.__larger_activities = list()
-    
-    def push(self, activity):
-        self.__larger_activities.append(activity)
-    
-    def pop(self):
-        return self.__larger_activities.pop()
-    
-    def peek(self):
-        return self.__larger_activities[self.size - 1]
-    
-    def size(self):
-        return len(self.__larger_activities)
-    
-    def to_set(self):
-        return set(self.__larger_activities)
-
 class ActivityOrderBuilder:
 
-    def __init__(self):
-        self.__is_larger_relations = dict()
+    def __init__(self, activities):
+        self.__is_larger_relations = {a:list() for a in activities}
     
     def add_relation(self, larger=None, smaller=None):
-        if smaller not in self.__is_larger_relations:
-            self.__is_larger_relations[smaller] = LargerActivityStack()
-        self.__is_larger_relations[smaller].push(larger)
+        self.__is_larger_relations[smaller].append(larger)
     
     def get_ordering(self):
         return ActivityOrder(self.__is_larger_relations)
