2d59cfd502adf454c2546942bfa8e756fd5eb24a
Yannick Epstein <yannick.epstein@gmail.com>
2019-04-09 12:08:44 +0200

    Outline for new post processing strategy

--- pm4py/algo/discovery/est_miner/hooks/post_processing_strategy.py
+++ pm4py/algo/discovery/est_miner/hooks/post_processing_strategy.py
@@ -16,3 +16,19 @@ class NoPostProcessingStrategy(PostProcessingStrategy):
     def execute(self, candidate_places):
         print('Executed Post Processing')
         return candidate_places
+
+class DeleteDuplicatePlacesPostProcessingStrategy(PostProcessingStrategy):
+
+    def execute(self, candidate_places):
+        return set(candidate_places)
+
+class ReplayBasedPostProcessingStrategy(PostProcessingStrategy):
+
+    def execute(self, candidate_places):
+        # Create net
+        # for each place
+        # remove place
+        # test if net still replays log
+        # if yes: keep place removed
+        # if no: re-add place
+        return candidate_places
