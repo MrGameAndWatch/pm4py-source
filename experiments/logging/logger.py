8352efb58f68193aaf0b3d7c87503d99a7f5ad67
Yannick Epstein <yannick.epstein@gmail.com>
2019-06-03 14:11:06 +0200

    Small changes in logging

--- experiments/logging/logger.py
+++ experiments/logging/logger.py
@@ -1,5 +1,6 @@
 from datetime import datetime, time
 import math
+import pickle
 
 from pm4py.algo.discovery.est_miner.utils.place import Place
 import pm4py.algo.discovery.est_miner.utils.constants as const
@@ -34,6 +35,30 @@ class RuntimeStatisticsLogger:
     def est_miner_name(self):
         return self._est_miner_name
     
+    @property
+    def algo_start_time(self):
+        return self._algo_start_time
+    
+    @property
+    def algo_finish_time(self):
+        return self._algo_finish_time
+    
+    @property
+    def search_start_time(self):
+        return self._search_start_time
+    
+    @property
+    def search_finish_time(self):
+        return self._search_finish_time
+    
+    @property
+    def post_proc_start_time(self):
+        return self._post_proc_start_time
+    
+    @property
+    def post_proc_finish_time(self):
+        return self._post_proc_fin_time
+    
     def replay_started(self):
         self._replay_start_time = self._now()
     
@@ -65,11 +90,11 @@ class RuntimeStatisticsLogger:
     def post_processing_finished(self):
         self._post_proc_fin_time = self._now()
     
-    def pruned_blue_place(self, place):
-        self._blue_pruned_places.append(place)
+    def pruned_blue_subtree(self, place):
+        self._blue_pruned_places.append(place.copy())
     
-    def pruned_red_place(self, place):
-        self._red_pruned_places.append(place)
+    def pruned_red_subtree(self, place):
+        self._red_pruned_places.append(place.copy())
     
     def algo_runtime(self, unit='ms'):
         delta = self._algo_finish_time - self._algo_start_time
@@ -98,16 +123,20 @@ class RuntimeStatisticsLogger:
     def total_pruned_places(self):
         return self.num_pruned_blue_places() + self.num_pruned_red_places()
     
+    def to_file(self, path):
+        with open(path, 'wb') as file:
+            pickle.dump(self, file)
+    
     def _blue_subtree_size(self, p):
-        a_max = max_element(p.output_trans, self._output_order)
+        a_max = max_element(self._transitions, p.output_trans, self._output_order)
         missing_output_trans = set(self._output_order.is_larger_relations[a_max]).difference({const.START_ACTIVITY})
         return pow(2, len(missing_output_trans)) - 1
     
     def _red_subtree_size(self, p):
-        if len(p.output_trans) > 1:
+        if p.num_output_trans > 1:
             return 0
         else:
-            a_max = max_element(p.input_trans, self._input_order)
+            a_max = max_element(self._transitions, p.input_trans, self._input_order)
             missing_input_trans = set(self._input_order.is_larger_relations[a_max]).difference({const.END_ACTIVITY})
             return pow(2, len(missing_input_trans)) - 1
 
